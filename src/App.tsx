22316683b9d17dfc55a088720ca9df6424a7e02b
Lara Beatriz <laraamori.machado@gmail.com>
2020-05-30 16:54:47 -0300

    :rocket: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <h1>Hello VUTTR!</h1>
+
+      <GlobalStyle />
+    </Router>
   );
-}
+};
 
 export default App;
